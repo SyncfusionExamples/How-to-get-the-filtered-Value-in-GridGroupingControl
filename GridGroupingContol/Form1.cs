d3f4ec659127791d231d76546752cedbe6feb049
Sweatha Bharathi <sweatha.bharathi@syncfusion.com>
2025-05-27 18:27:53 +0530

    How to get the filtered Value in GridGroupingControl.

---
+++ GridGroupingContol/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Syncfusion.GridHelperClasses;
+using Syncfusion.Windows.Forms.Grid;
+using Syncfusion.Windows.Forms.Grid.Grouping;
+
+namespace GridGroupingControl_Demo4_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Create a new DataTable
+            DataTable dt = new DataTable();
+
+            // Add 5 columns: mix of int and string
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Age", typeof(int));
+            dt.Columns.Add("City", typeof(string));
+            dt.Columns.Add("Score", typeof(int));
+
+            // Add 10 rows of sample data
+            for (int i = 1; i <= 10; i++)
+            {
+                dt.Rows.Add(i, $"Name{i}", 20 + i, $"City{i}", 80 + i);
+            }
+
+            // Bind the DataTable to GridGroupingControl
+            gridGroupingControl1.DataSource = dt;
+
+            GridExcelFilter gridExcelFilter = new GridExcelFilter();
+
+            // Allowing Filter Bar for all columns in the Parent Table.
+            foreach (GridColumnDescriptor col in this.gridGroupingControl1.TableDescriptor.Columns)
+            {
+                col.AllowFilter = true;
+            }
+
+            //Wiring GridExcelFilter to GridGroupingControl
+            gridExcelFilter.WireGrid(this.gridGroupingControl1);
+
+            gridExcelFilter.RecordFiltersItemChanged += Filter_RecordFiltersItemChanged;
+
+        }
+
+        private void Filter_RecordFiltersItemChanged(object sender, Syncfusion.Collections.ListPropertyChangedEventArgs e)
+        {
+            var filteredValues = (e.Tag as Dictionary<string, System.Collections.Generic.List<string>>).Values;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# How-to-get-the-filtered-Value-in-GridGroupingControl
-This demo shows how to get the filtered Value in GridGroupingControl
+# How to get the filtered Value in GridGroupingControl
+
+In WinForms [GridGroupingControl](https://help.syncfusion.com/windowsforms/gridgrouping/overview), you can retrieve the filtered values by using the [RecordFiltersItemChanged](https://help.syncfusion.com/cr/windowsforms/Syncfusion.GridHelperClasses.GridExcelFilter.html#Syncfusion_GridHelperClasses_GridExcelFilter_RecordFiltersItemChanged) event. This event is triggered after a filter is applied to the **GridGroupingControl**.
+
+ ```C#
+gridExcelFilter.RecordFiltersItemChanged += GridExcelFilter_RecordFiltersItemChanged;
+
+private void Filter_RecordFiltersItemChanged(object sender, Syncfusion.Collections.ListPropertyChangedEventArgs e)
+{
+   var filteredValues = (e.Tag as Dictionary<string, System.Collections.Generic.List<string>>).Values;
+}
+
+ ```
+Take a moment to peruse the [Winforms GridGroupingControl - RecordFiltersItemChanged](https://help.syncfusion.com/windowsforms/gridgrouping/filtering#events-2) event documentation, where you can find about the filtering with code examples.
